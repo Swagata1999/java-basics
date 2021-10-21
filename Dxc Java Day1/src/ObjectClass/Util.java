9ae0f68b0bb99a97c55eda2739f9e90ee1eb631e
Swagata1999 <swagatadas1999@gmail.com>
2021-10-21 13:57:02 +0530

    Static Variables

---
+++ Dxc Java Day1/src/ObjectClass/Util.java
@@ -0,0 +1,16 @@
+package ObjectClass;
+
+/**
+ * this class has all utility methods
+ * @author Admin
+ *
+ */
+public class Util {
+	
+	public static String COMPANY_NAME = "dxc";
+	
+	public static int incrementByTen(int no) {
+		return no + 10;
+	}
+
+}
