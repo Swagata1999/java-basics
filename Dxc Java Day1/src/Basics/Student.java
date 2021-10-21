7b405f8e73e83f5db9e335f0fd4cc0b119b95d5c
Swagata1999 <swagatadas1999@gmail.com>
2021-10-21 14:08:24 +0530

    Inheritance

---
+++ Dxc Java Day1/src/Basics/Student.java
@@ -0,0 +1,69 @@
+package Basics;
+
+import ObjectClass.Util;
+
+public class Student {
+	
+	String name; //partition 
+	int age; // 
+	
+	/**
+	 * default constructor -- harshita
+	 */
+	public Student() {
+		System.out.println("harshita constructor");
+	}
+	
+	
+	/**
+	 * lahari constructor
+	 * @param name
+	 */
+	public Student(String name) {
+		this.name = name;
+		System.out.println("lahari constructor");
+	}
+	
+	/**
+	 * this is sathvika constructor
+	 * @param name
+	 * @param age
+	 */
+	public Student(String name, int age) {
+		this(name);
+		System.out.println("satvika constructor");
+		//this.name = name;
+		this.age = Util.incrementByTen(age);
+		displayStudent(this);
+	}
+
+	static String COLLEGE_NAME = "IIT";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	void displayStudent(Student s) {
+		System.out.println("students name is  "+s.name);
+	}
+
+	
+	
+	//Student[] students = new Student[100];
+	
+	
+
+}
