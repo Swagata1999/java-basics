d81519f56559f8ceecc732c58050cff7cbb2ee4e
Swagata1999 <swagatadas1999@gmail.com>
2021-10-26 10:57:06 +0530

    ArrayList

---
+++ Dxc Java Day1/src/Collections/DemoOnArraylist.java
@@ -0,0 +1,15 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoOnArraylist {
+	
+	public static void main(String[] args) {
+		List<String> names = new ArrayList<String>();
+		names.add("Swagata");
+		//names.add(30);
+		System.out.println(names);
+	}
+
+}
